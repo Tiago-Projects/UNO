71f82313dc511791e8475761854c45b9f3e2e81f
Tiago Rodrigues <tiago.andre764@hotmail.com>
2025-04-30 16:26:21 +0100

    CardMapper tests implemented.

---
+++ backend/src/test/java/com/uno/Uno/Mapper/CardMapperTest.java
@@ -0,0 +1,26 @@
+package com.uno.Uno.Mapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.uno.Uno.Dto.CardDto;
+import com.uno.Uno.Model.CardModel;
+import com.uno.Uno.Model.Enum.Suit;
+import com.uno.Uno.Model.Enum.Type;
+
+@SpringBootTest
+public class CardMapperTest {
+    
+    @Test
+    public void testToDto() {
+        CardModel card = new CardModel(Type.ZERO, Suit.BLUE);
+
+        CardDto cardDto = CardMapper.toDto(card);
+
+        assertEquals(cardDto.getSuit(), card.getSuit());
+        assertEquals(cardDto.getType(), card.getType());
+    } 
+
+}
